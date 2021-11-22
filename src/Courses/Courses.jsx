48b134a22f544452a62b978c793f695c14538ab8
Htut Aung Wai <htutaungwai010@gmail.com>
2021-11-22 16:41:58 +0630

    added apply course page without validation

--- src/Courses/Courses.jsx
+++ src/Courses/Courses.jsx
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import background from '../dummyImages/dummyLongBackground.jpg';
+import classes from './Courses.module.css';
+import { Row, Col, Typography, Form, Button } from 'antd';
+import FormItem from './FormItem';
+
+const leftBackground = { 
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  height: '800px',
+}
 
 export const Courses = () => {
-  return <div>Courses</div>;
-};
+
+  const [ inputs, setInputs ] = useState();
+
+  const onFinish = values => {
+    setInputs(values)
+    console.log(values)
+  }
+
+  return (
+    <>
+      <Row>
+        <Col md={8}>
+          <div style={leftBackground}></div>
+        </Col>
+        <Col md={16} className={classes.rightPanel}>
+        <Typography.Text className={classes.title}>Apply for Course</Typography.Text>
+        <div className={classes.wrapper}>
+          <Form colon={false} onFinish={onFinish} requiredMark={false}>
+            <Row justify='space-between'>
+              <Col md={12} sm={24}>
+                <FormItem label="Name" name='name'/>
+                <FormItem label="Phone Number" name='phoneNumber'/>
+                <FormItem label="Occupancy" name='occupancy' />
+                <FormItem label="Gender" name='gender' />
+              </Col>
+              <Col md={12} sm={24}>
+                <FormItem label="Age" name='age' />
+                <FormItem label="E-mail" name='email' />
+                <FormItem label="Company/School Name" name='company/school' />
+                <FormItem label="Desired Course" name='desiredCourse' />
+              </Col>
+              <Button type="primary" size='large' style={{width: '35%', borderRadius: '10px'}} htmlType="submit">Apply</Button>
+            </Row>
+          </Form>
+        </div>
+        </Col>
+      </Row>
+    </>
+  )
+}
---
+++ src/Courses/FormItem.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import classes from './Courses.module.css'
+import { Form, Radio, Input, Select } from 'antd'
+
+const FormItem = ({ label, name }) => {
+
+    let message = '';
+    switch(name) {
+        case 'name': message='Please fill your name'; break;
+        case 'email': message='Please fill the email'; break;
+        case 'phoneNumber': message='Please fill the phone number'; break;
+        case 'age': message='Please fill the age'; break;
+        case 'occupancy': message='Please fill the occupancy'; break;
+        case 'company/school': message='Please fill the company or school'; break;
+        case 'desiredCourse': message='Please fill the desired course'; break;
+        case 'gender': message='Please select the gender'; break;
+        default: message = '';break;
+    }
+    const rules=[
+        {
+            type: name === 'email' && 'email',
+            required: true,
+            message: message,
+        },
+    ]
+
+    const formItems = () => {
+        if(name === 'gender') {
+            return (
+                <Form.Item label="Gender" name='gender' labelCol={{sm: 24}} labelAlign='left' rules={rules}>
+                    <Radio.Group>
+                        <Radio value="male">Male</Radio>
+                        <Radio value="female">Female</Radio>
+                    </Radio.Group>
+                </Form.Item>
+            )
+        } else if(name === 'desiredCourse') {
+            return (
+                <Form.Item label="Desired Course" name='desired course' labelCol={{sm: 24}} labelAlign='left' rules={rules}>
+                    <Select size='large' style={{ width: 420 }}>
+                        <Select.Option value="course one">Course 1</Select.Option>
+                        <Select.Option value="course two">Course 2</Select.Option>
+                        <Select.Option value="course three">Course 3</Select.Option>
+                    </Select>
+                </Form.Item>
+            )
+        } else {
+            return (
+                <Form.Item label={label} name={name} labelCol={{sm: 24}} labelAlign='left' rules={rules}>
+                    <Input size='large' className={classes.input} style={{ width: '80%' }}/>
+                </Form.Item>
+            )
+        }
+    }
+
+    return (
+        <>
+            {formItems()}
+        </>
+    )
+}
+
+export default FormItem
